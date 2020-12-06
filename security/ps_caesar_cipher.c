2118d0ec4de680432f5f1edff5ff228337f07416
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-06 13:46:18 +0900

    Update ps_caesar_cipher.c

--- security/ps_caesar_cipher.c
+++ security/ps_caesar_cipher.c
@@ -8,8 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-#define BUFFER_SIZE 23
                            // 0    1    2    3    4    5    6    7    8    9   10   11   12  
 static char alphabet[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                            //13   14   15   16   17   18   19   20   21   22   23   24   25     
