2df6e25661f94fb14c741e4d6bd83b065db11468
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-06 19:23:20 +0900

    Update ps_function_in_struct.c

--- general/ps_function_in_struct.c
+++ general/ps_function_in_struct.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-// minimize useage of (/ → * → - → +) operator and floating-point
+// minimize usage of (/ → * → - → +) operator and floating-point
 // New Struct Here! -------------------------------------------------------------
 typedef struct example{
     // struct variables Here!
@@ -17,7 +17,7 @@ typedef struct example{
     void (* push_number)(int number);
 } Example;
 
-Example * __ref = NULL; // All functions related to Example can use this pointer.
+Example * __ref; // All functions related to Example can use this pointer.
 
 // Explanation of Functions ------------------------------------------------------------
 // functions with struct example(Example) -----------------------------------
@@ -40,25 +40,25 @@ int main(void) {
                   push_number_st // functions
                 }; // initializing(constructor)    
     
-__ref = &a; // for Example functions(must): useage of Example a
+__ref = &a; // for Example functions(must): usage of Example a
     a.push_number(1); // a.number = 1
     printf("a.number = %d\n", a.number);    
     a.push_number(2); // a.number = 2
     printf("a.number = %d\n", a.number);
 
-__ref = &b; // for Example functions(must): useage of Example b
+__ref = &b; // for Example functions(must): usage of Example b
     b.push_number(3); // b.number = 3 
     printf("b.number = %d\n", b.number);
     b.push_number(4); // b.number = 4
     printf("b.number = %d\n", b.number);    
     
-__ref = &c; // for Example functions(must): useage of Example c    
+__ref = &c; // for Example functions(must): usage of Example c    
     c.push_number(5); // c.number = 5
     printf("c.number = %d\n", c.number);    
     c.push_number(6); // c.number = 6 
     printf("c.number = %d\n", c.number);
     
-__ref = &a; // for Example functions(must): useage of Example a
+__ref = &a; // for Example functions(must): usage of Example a
     a.push_number(5); // a.number = 1
     printf("a.number = %d\n", a.number);    
     a.push_number(6); // a.number = 2
