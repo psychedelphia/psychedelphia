1adaaffd30f5411b48a06f6fa9a5932c4cabdf66
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-06 10:12:42 +0900

    Create ps_function_in_struct.c

---
+++ general/ps_function_in_struct.c
@@ -0,0 +1,74 @@
+//-------------------------------------------------------------//
+// Forever a Student in C                                      //
+// Psychedelphia: https://psychedelphia.wordpress.com/         //
+//-------------------------------------------------------------//
+// Implementation of Array List                                //
+//-------------------------------------------------------------//
+
+#include <stdio.h>
+
+// minimize useage of (/ → * → - → +) operator and floating-point
+// New Struct Here! -------------------------------------------------------------
+typedef struct example{
+    // struct variables Here!
+    int number;
+    
+    // struct functions Here!
+    void (* push_number)(int number);
+} Example;
+
+Example * __ref; // All functions related to Example can use this pointer.
+
+// Explanation of Functions ------------------------------------------------------------
+// functions with struct example(Example) -----------------------------------
+//-----------------------------
+// push_number() implementation: insert value into local variable 
+void push_number_st(int number) { // function name + "_st" (mean: for struct)
+    __ref->number = number;
+}
+
+//----------------------
+// main() implementation 
+int main(void) {
+    Example a = { 0,             // variables
+                  push_number_st // functions 
+                }; // initializing(constructor)
+    Example b = { 0,             // variables
+                  push_number_st // functions
+                }; // initializing(constructor)
+    Example c = { 0,             // variables
+                  push_number_st // functions
+                }; // initializing(constructor)    
+    
+__ref = &a; // for Example functions(must): useage of Example a
+    a.push_number(1); // a.number = 1
+    printf("a.number = %d\n", a.number);    
+    a.push_number(2); // a.number = 2
+    printf("a.number = %d\n", a.number);
+
+__ref = &b; // for Example functions(must): useage of Example b
+    b.push_number(3); // b.number = 3 
+    printf("b.number = %d\n", b.number);
+    b.push_number(4); // b.number = 4
+    printf("b.number = %d\n", b.number);    
+    
+__ref = &c; // for Example functions(must): useage of Example c    
+    c.push_number(5); // c.number = 5
+    printf("c.number = %d\n", c.number);    
+    c.push_number(6); // c.number = 6 
+    printf("c.number = %d\n", c.number);
+    
+__ref = &a; // for Example functions(must): useage of Example a
+    a.push_number(5); // a.number = 1
+    printf("a.number = %d\n", a.number);    
+    a.push_number(6); // a.number = 2
+    printf("a.number = %d\n", a.number);    
+    
+    if (a.number == c.number) { printf("equal!\n"); }
+    else { printf("not equal!\n"); }
+    
+    if (b.number == c.number) { printf("equal!\n"); }
+    else { printf("not equal!\n"); }
+    
+    return 0; 
+}
