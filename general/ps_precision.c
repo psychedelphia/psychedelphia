400dc915fd58ced5f5f3ee7fda5abed22f9c661a
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-06 19:21:08 +0900

    Update ps_precision.c

--- general/ps_precision.c
+++ general/ps_precision.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-// minimize useage of (/ → * → - → +) operator and floating-point
+// minimize usage of (/ → * → - → +) operator and floating-point
 // New Function Here! -------------------------------------------
 void precision(double number, int value); // double precision
 // --------------------------------------------------------------
