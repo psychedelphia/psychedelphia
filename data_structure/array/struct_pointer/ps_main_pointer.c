f87d763f9af28eb8f0336756996bb070dd2cb7d9
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-10 10:16:46 +0900

    Update ps_main_pointer.c

--- data_structure/array/struct_pointer/ps_main_pointer.c
+++ data_structure/array/struct_pointer/ps_main_pointer.c
@@ -14,71 +14,65 @@
 //----------------------
 // main() implementation 
 int main(void) {
-    Array a = { { 0 }, 0, 
-                init_st,     push_front_st, push_back_st, 
-                insert_st,   erase_st,      assign_st, 
-                swap_st,     front_st,      back_st,     
-                at_st,       print_st,      capacity_st, 
-                size_st,     rest_st,       empty_st,
-              };  // initializing(constructor)
+    Array a = { { 0 }, 0 };  // initializing(constructor)
 __array_ptr = &a; // for Array functions(must)
     // example
-    printf("capacity: "); /* << */ a.capacity(); /* << */ puts("");  
-    a.print(); /* << */ puts("");
+    printf("capacity: "); /* << */ capacity_st(); /* << */ puts("");  
+    print_st(); /* << */ puts("");
     printf("insert data\n");
-    a.push_front(1);
-    a.print(); /* << */ puts("");
+    push_front_st(1);
+    print_st(); /* << */ puts("");
     printf("insert data\n");
-    a.push_front(2);
-    a.print(); /* << */ puts("");
+    push_front_st(2);
+    print_st(); /* << */ puts("");
 
     printf("insert data\n");    
-    a.push_back(3);
-    a.print(); /* << */ puts("");
+    push_back_st(3);
+    print_st(); /* << */ puts("");
     printf("insert data\n");
-    a.push_back(4);
-    a.print(); /* << */ puts("");    
-    printf("first data: "); /* << */ a.front(); /* << */ puts("");
-    printf("last data: "); /* << */ a.back(); /* << */ puts("");
+    push_back_st(4);
+    print_st(); /* << */ puts("");    
+    printf("first data: "); /* << */ front_st(); /* << */ puts("");
+    printf("last data: "); /* << */ back_st(); /* << */ puts("");
     
-    printf("value of data[2]: "); /* << */ a.at(2); /* << */ puts("");
-    printf("value of data[3]: "); /* << */ a.at(3); /* << */ puts("");
+    printf("value of data[2]: "); /* << */ at_st(2); /* << */ puts("");
+    printf("value of data[3]: "); /* << */ at_st(3); /* << */ puts("");
 
     printf("swap data\n");
-    a.swap(1, 4);    
-    a.print(); /* << */ puts("");
+    swap_st(1, 4);    
+    print_st(); /* << */ puts("");
     printf("swap data\n");    
-    a.swap(2, 3);    
-    a.print(); /* << */ puts("");
+    swap_st(2, 3);    
+    print_st(); /* << */ puts("");
 
     printf("assign data\n");
-    a.assign(5, 3);
-    a.print(); /* << */ puts("");
+    assign_st(5, 3);
+    print_st(); /* << */ puts("");
 
     printf("insert data\n");
-    a.insert(4, 4);
-    a.print(); /* << */ puts("");
+    insert_st(4, 4);
+    print_st(); /* << */ puts("");
     printf("insert data\n");
-    a.insert(5, 5);
-    a.print(); /* << */ puts("");
+    insert_st(5, 5);
+    print_st(); /* << */ puts("");
 
     printf("erase data\n");
-    a.erase(2);
-    a.print(); /* << */ puts("");
+    erase_st(2);
+    print_st(); /* << */ puts("");
     printf("erase data\n");
-    a.erase(6);
-    a.print(); /* << */ puts("");
+    erase_st(6);
+    print_st(); /* << */ puts("");
 
-    printf("rest of array: "); /* << */ a.rest(); /* << */ puts("");
-    printf("size: "); /* << */ a.size(); /* << */ puts("");
+    printf("rest of array: "); /* << */ rest_st(); /* << */ puts("");
+    printf("size: "); /* << */ size_st(); /* << */ puts("");
     
-    if (a.empty()) { printf("array is empty\n"); }
+    if (empty_st()) { printf("array is empty\n"); }
     else { printf("array is not empty\n"); }
     
     printf("initializing\n");
-    a.init();
-    printf("size: "); /* << */ a.size(); /* << */ puts("");
-    if (a.empty()) { printf("array is empty\n"); }
+    init_st();
+    printf("size: "); /* << */ size_st(); /* << */ puts("");
+    if (empty_st()) { printf("array is empty\n"); }
     else { printf("array is not empty\n"); }    
     
     return 0; 
