d1768340d4f8a1e2b615da17c147587502ba7593
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-11 21:25:55 +0900

    Create ps_datatypes.h

---
+++ ps_datatypes.h
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------//
+// Forever a Student in C                                      //
+// Psychedelphia: https://psychedelphia.wordpress.com/         //
+//-------------------------------------------------------------//
+// Data Types                                                  //
+//-------------------------------------------------------------//
+
+#define SUCCESS 0
+
+// Data Types -------------------------------------------------------------------
+typedef enum boolean { FALSE, TRUE }     bool;
+typedef unsigned char                   uchar;
+typedef short int                       int16;
+typedef unsigned short int             uint16;
+typedef int                             int32;
+typedef unsigned int                   uint32;
+typedef long int                        int64;
+typedef unsigned long int              uint64;
+typedef float                         float32;
+typedef double                        float64;
+typedef long double                  float128;
+//-------------------------------------------------------------------------------
