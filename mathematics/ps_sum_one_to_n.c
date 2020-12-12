e9f7029c3d61a767ec8d7d7adf22e29f028dddaa
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-12 10:15:12 +0900

    Update ps_sum_one_to_n.c

--- mathematics/ps_sum_one_to_n.c
+++ mathematics/ps_sum_one_to_n.c
@@ -6,7 +6,6 @@
 //-------------------------------------------------------------//
 
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 #include "ps_datatypes.h"
@@ -15,32 +14,35 @@
 //-------------------------------------------------------------------------------
 
 // New Function Here! -----------------------------------------------------------
-void sum(int32 one, int32 max); // 1 + 2 + … + n
+void sum(int32 max); // 1 + 2 + … + n
 //-------------------------------------------------------------------------------
 
 // minimize usage of (/, *, -, +) operator and floating-point
 //----------------------
 // main() implementation 
 int32 main(void) {
-    int32 one, max;
+    int32 max;
      
     srand(time(NULL));
      
-    one = 1;
     max = rand() >> 24;
      
     // sum = 1 + 2 + ... + n
-    printf("1 ~ %d: ", max); /* << */ sum(one, max); /* << */ puts("");
+    printf("1 ~ %d: ", max); /* << */ sum(max); /* << */ puts("");
      
     return SUCCESS; 
 }
  
 //-----------------------
 // sum() implementation
-void sum(int32 one, int32 max) {
+void sum(int32 max) {
     int32 sum;
-
-    sum = (one + max) * (max >> 1);
-  
-    printf("case 1: sum = %d", sum);
+    
+    if (max >= 1) { // execution
+        // (1 + max) * (max / 2)
+        sum = (1 + max) * (max >> 1);
+        printf("case 1: sum = %d", sum);        
+    } else { // exception
+        printf("out of bound!");
+    }
 }
