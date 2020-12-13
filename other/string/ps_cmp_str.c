9ece06c34aaf83b99b492c87d5261231fafb7bac
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-13 09:32:58 +0900

    Update ps_cmp_str.c

--- other/string/ps_cmp_str.c
+++ other/string/ps_cmp_str.c
@@ -9,14 +9,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ps_datatypes.h"
-
-// Global variable Here! --------------------------------------------------------
-//-------------------------------------------------------------------------------
+#include "ps_string.h"
 
 // New Function Here! -----------------------------------------------------------
 int32 cmp_str(const char * str1, const char * str2); // print string
-
-char * init_str_h(const char * str); // create string
 //-------------------------------------------------------------------------------
 
 // minimize usage of (/ → * → - → +) operator and floating-point
@@ -26,12 +22,16 @@ int32 main(void) {
     char * str1 = init_str_h("This is a string");
     char * str2 = init_str_h("What happened?");
     char * str3 = init_str_h("What happened?");
-    char * str4 = init_str_h("Whaa happened?");    
+    char * str4 = init_str_h("Whaa happened?");
+    char * str5 = NULL;
+    char * str6 = NULL;
     int32 ret;
     
     ret = cmp_str(str1, str2);
     
-    if (ret > 0) {
+    if (ret == 0) {
+        printf("two string are same.\n");
+    } else if (ret > 0) {
         printf("left string > right string\n");
     } else {
         printf("left string < right string\n");
@@ -41,26 +41,48 @@ int32 main(void) {
     
     if (ret == 0) {
         printf("two string are same.\n");
-    } else {
-        printf("two string are not same.\n");
-    }
-    
-    ret = cmp_str(str3, str4);
-    
-    if (ret > 0) {
+    } else if (ret > 0) {
         printf("left string > right string\n");
     } else {
         printf("left string < right string\n");
     }
     
-    free(str1);
-    str1 = NULL;
-    free(str2);
-    str2 = NULL;
-    free(str3);
-    str3 = NULL;
-    free(str4);
-    str4 = NULL;    
+    ret = cmp_str(str3, str4);
+    
+    if (ret == 0) {
+        printf("two string are same.\n");
+    } else if (ret > 0) {
+        printf("left string > right string\n");
+    } else {
+        printf("left string < right string\n");
+    }
+    
+    ret = cmp_str(str4, str5);
+    
+    if (ret == 0) {
+        printf("two string are same.\n");
+    } else if (ret > 0) {
+        printf("left string > right string\n");
+    } else {
+        printf("left string < right string\n");
+    }
+
+    ret = cmp_str(str5, str6);
+    
+    if (ret == 0) {
+        printf("two string are same.\n");
+    } else if (ret > 0) {
+        printf("left string > right string\n");
+    } else {
+        printf("left string < right string\n");
+    }
+    
+    str1 = term_str(str1);
+    str2 = term_str(str2);
+    str3 = term_str(str3);
+    str4 = term_str(str4);
+    str5 = term_str(str5);
+    str6 = term_str(str6);    
     
     return SUCCESS; 
 }
@@ -80,31 +102,14 @@ int32 cmp_str(const char * str1, const char * str2) {
             result = len1 < len2 ? -1 : 1;
         }
     } else { // exception
-        printf("NULL found!");    
+        if (str1 == NULL && str2 == NULL) {
+            result = 0;
+        } else if (str1 != NULL && str2 == NULL) {
+            result = 1;
+        } else {
+            result = -1;
+        }
     }
     
     return result;
-}
-
-char * init_str_h(const char * str) { // function name + "_h" (mean: heap memory(free() needed))
-    char * copied;
-    uint64 length;
-
-    if (str != NULL) { // execution
-        length = strlen(str);        
-    } else { // exception
-        length = 0;
-    }
-    
-    copied = (char * )malloc(sizeof(char) * (length + 1));
-
-    if (copied != NULL) { // execution
-        if (length) { memcpy(copied, str, length); }
-    
-        *(copied + length) = '\0';
-    } else { // exception
-        printf("memory is not enough!");
-    }
-
-    return copied;
 }
