783bee239963fea1b7059e6c854b8175c62d6923
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-13 09:45:10 +0900

    Update ps_print_str.c

--- other/string/ps_print_str.c
+++ other/string/ps_print_str.c
@@ -9,11 +9,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ps_datatypes.h"
+#include "ps_string.h"
 
 // New Function Here! -----------------------------------------------------------
 void print_str(const char * str); // print string
-
-char * init_str_h(const char * str); // create string
 //-------------------------------------------------------------------------------
 
 // minimize usage of (/ → * → - → +) operator and floating-point
@@ -26,37 +25,12 @@ int32 main(void) {
     printf("string1 created: "); /* << */ print_str(str1); /* << */ puts("");
     printf("string2 created: "); /* << */ print_str(str2); /* << */ puts("");
 
-    free(str1);
-    str1 = NULL;
-    free(str2);
-    str2 = NULL;
+    str1 = term_str(str1);
+    str2 = term_str(str2);
     
     return SUCCESS; 
 }
 
 void print_str(const char * str) {
     printf("%s", str);
-}
-
-char * init_str_h(const char * str) { // function name + "_h" (mean: heap memory(free() needed))
-    char * copied;
-    uint64 length;
-
-    if (str != NULL) { // execution
-        length = strlen(str);        
-    } else { // exception
-        length = 0;
-    }
-    
-    copied = (char * )malloc(sizeof(char) * (length + 1));
-
-    if (copied != NULL) { // execution
-        if (length) { memcpy(copied, str, length); }
-    
-        *(copied + length) = '\0';
-    } else { // exception
-        printf("memory is not enough!");
-    }
-
-    return copied;
 }
