1ed4f520af0f43e4185e53e0f767033b1cc0f381
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-13 09:16:35 +0900

    Create ps_string.h

---
+++ other/string/ps_string.h
@@ -0,0 +1,4 @@
+// initialize and terminate string ----------------------------------------------
+char * init_str_h(const char * str); // create string
+char * term_str(char * str); // terminate string
+//-------------------------------------------------------------------------------
