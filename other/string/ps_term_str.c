f6159949103d958f1410177fd1d50fb588ca34be
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-13 09:40:22 +0900

    Update ps_term_str.c

--- other/string/ps_term_str.c
+++ other/string/ps_term_str.c
@@ -9,9 +9,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include "ps_datatypes.h"
+#include "ps_string.h"
 
 // New Function Here! -----------------------------------------------------------
-char * init_str_h(const char * str); // create string
 char * term_str(char * str); // terminate string
 //-------------------------------------------------------------------------------
 
@@ -41,27 +41,4 @@ char * term_str(char * str) {
     removed = NULL;
  
     return removed;   
-}
-
-char * init_str_h(const char * str) { // function name + "_h" (mean: heap memory(free() needed))
-    char * copied;
-    uint64 length;
-
-    if (str != NULL) { // execution
-        length = strlen(str);        
-    } else { // exception
-        length = 0;
-    }
-    
-    copied = (char * )malloc(sizeof(char) * (length + 1));
-
-    if (copied != NULL) { // execution
-        if (length) { memcpy(copied, str, length); }
-    
-        *(copied + length) = '\0';
-    } else { // exception
-        printf("memory is not enough!");
-    }
-
-    return copied;
 }
