666604aab789d209db388d831f00480cddd62338
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-14 19:22:34 +0900

    Update ps_string.h

--- other/string/ps_string.h
+++ other/string/ps_string.h
@@ -5,3 +5,4 @@ char * term_str(char * str); // terminate string
 
 void print_str(const char * str); // print string
 void printn_str(const char * str); // print string(+ newline)
+void length_str(const char * str); // length of string
