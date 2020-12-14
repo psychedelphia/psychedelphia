7d718d3f5910b91af4b087af4f72db4f40ebac4f
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-14 20:52:54 +0900

    Update ps_datatypes.h

--- ps_datatypes.h
+++ ps_datatypes.h
@@ -2,38 +2,27 @@
 // Forever a Student in C                                      //
 // Psychedelphia: https://psychedelphia.wordpress.com/         //
 //-------------------------------------------------------------//
-// size of data types                                          //
+// Data Types                                                  //
 //-------------------------------------------------------------//
 
-#include <stdio.h>
-#include "ps_datatypes.h"
+#include <uchar.h>
+#include <wchar.h>
 
-// minimize usage of (/ → * → - → +) operator and floating-point
-// New Function Here! -----------------------------------------------------------
-void show_sizeof();
+#define SUCCESS 0
+
+// Data Types -------------------------------------------------------------------
+typedef enum boolean { FALSE, TRUE }     bool;
+typedef unsigned char                   uchar;
+typedef wchar_t                         wchar;
+typedef char16_t                       char16;
+typedef char32_t                       char32;
+typedef short int                       int16;
+typedef unsigned short int             uint16;
+typedef int                             int32;
+typedef unsigned int                   uint32;
+typedef long int                        int64;
+typedef unsigned long int              uint64;
+typedef float                         float32;
+typedef double                        float64;
+typedef long double                  float128;
 //-------------------------------------------------------------------------------
-
-//----------------------
-// main() implementation 
-int main(void) {
-    show_sizeof();
-
-    return SUCCESS; 
-}
-
-void show_sizeof() {
-    printf("char     size: %ld\n", sizeof(char));
-    printf("uchar    size: %ld\n", sizeof(uchar));
-    printf("char16   size: %ld\n", sizeof(char16));
-    printf("int16    size: %ld\n", sizeof(int16));
-    printf("uint16   size: %ld\n", sizeof(uint16));
-    printf("wchar    size: %ld\n", sizeof(wchar));
-    printf("char32   size: %ld\n", sizeof(char32));    
-    printf("int32    size: %ld\n", sizeof(int32));
-    printf("uint32   size: %ld\n", sizeof(uint32));
-    printf("float32  size: %ld\n", sizeof(float32));
-    printf("int64    size: %ld\n", sizeof(int64));
-    printf("uint64   size: %ld\n", sizeof(uint64));
-    printf("float64  size: %ld\n", sizeof(float64));
-    printf("float128 size: %ld\n", sizeof(float128));    
-}
