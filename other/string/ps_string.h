2e3262604110a577743e86e1bed2ba6714440c8b
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-16 12:27:03 +0900

    Update ps_string.h

--- other/string/ps_string.h
+++ other/string/ps_string.h
@@ -5,6 +5,7 @@ char * term_str(char * str); // terminate string
 
 void print_str(const char * str); // print string
 void printn_str(const char * str); // print string(+ newline)
+void address_str(const char * str); // address of string
 void length_str(const char * str); // length of string
 
 bool numeric_str(const char * str); // numeric string or not
