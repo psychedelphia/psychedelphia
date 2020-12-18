381a3e10dafe4cc9a5640f439f9074800b768ea7
psychedelphia <74445567+psychedelphia@users.noreply.github.com>
2020-12-18 20:41:25 +0900

    Update ps_apnd_str_h.c

--- other/string/ps_apnd_str_h.c
+++ other/string/ps_apnd_str_h.c
@@ -2,7 +2,7 @@
 // Forever a Student in C                                      //
 // Psychedelphia: https://psychedelphia.wordpress.com/         //
 //-------------------------------------------------------------//
-// append new string                                           //
+// append string                                               //
 //-------------------------------------------------------------//
 
 #include <stdio.h>
@@ -22,147 +22,188 @@ char * apnd_isub_str_h(char * this, const char * str, int32 index, int32 len); /
 //----------------------
 // main() implementation
 int32 main(void) {
-    char * str1 = init_str_h("This is a string");
-    char * str2 = init_str_h("after");
-
+    char * str1 = NULL;
+    char * str2 = init_str_h("This is a string");
+    char * str3 = init_str_h("after");
+    
+    printn_str(str1);
+    
     str1 = apnd_str_h(str1, str2);
-    print_str(str1); /* << */ puts("");    
+    printn_str(str1);
 
-    str2 = apnd_char_str_h(str2, '$', 5);
-    print_str(str2); /* << */ puts("");    
+    str1 = apnd_char_str_h(str1, '+', 3);
+    printn_str(str1);
 
-    str1 = apnd_fsub_str_h(str1, str2, 3);
-    print_str(str1); /* << */ puts("");
+    str1 = apnd_fsub_str_h(str1, str3, 2);
+    printn_str(str1);
 
-    str2 = apnd_isub_str_h(str2, str1, 5, 4);
-    print_str(str2); /* << */ puts("");
+    str1 = apnd_isub_str_h(str1, str1, 2, 4);
+    printn_str(str1);    
     
     str1 = term_str(str1);
     str2 = term_str(str2);
+    str3 = term_str(str3);    
     
     return SUCCESS; 
 }
 
 char * apnd_str_h(char * this, const char * str) { // function name + "_h" (meaning: heap memory(free() needed))
-    char * append = NULL;
+    char * temp = NULL;
     uint64 thislength, strlength;
 
-    if (str != NULL) { // execution
+    if (str) { // str != NULL
         strlength = strlen(str);
-        
-        if (this != NULL) { thislength = strlen(this); } 
-        else              { thislength = 0; }
     
-        append = (char * )malloc(sizeof(char) * (thislength + strlength + 1));
-    
-        if (append != NULL) { // execution
-            if (this != NULL) { memcpy(append, this, thislength); }
+        if (this) { // this != NULL
+            thislength = strlen(this);
 
-            memcpy(append + thislength, str, strlength);
-            *(append + thislength + strlength) = '\0';
+            temp = this;
+            this = (char * )realloc(this, sizeof(char) * (thislength + strlength + 1));
+
+            if (this) { // execution
+                memcpy(this, temp, thislength);
+                memcpy(this + thislength, str, strlength);
+                *(this + thislength + strlength) = '\0';
+            } else { // exception
+                temp = term_str(temp);
+                printf("memory is not allocated!");
+                exit(EXIT_FAILURE);
+            }
+        } else { // this == NULL
+            this = (char * )malloc(sizeof(char) * (strlength + 1));
             
-            this = term_str(this); // destructed original pointer
-        } else { // exception
-            printf("memory is not allocated!");
-            exit(EXIT_FAILURE);
+            if (this) { // execution
+                memcpy(this, str, strlength);
+                *(this + strlength) = '\0';
+            } else { // exception
+                printf("memory is not allocated!");
+                exit(EXIT_FAILURE);
+            }            
         }
-    } else { // exception
-        append = this;
-    } 
-
-    return append;   
+    }
+    
+    return this;   
 }
 
 char * apnd_char_str_h(char * this, const char c, int num) { // function name + "_h" (meaning: heap memory(free() needed))
-    char * append = NULL;
+    char * temp = NULL;
     uint64 thislength;
 
-    if (this != NULL) { thislength = strlen(this); } 
-    else              { thislength = 0; }
-    
-    append = (char * )malloc(sizeof(char) * (thislength + num + 1));
-    
-    if (append != NULL) { // execution
-        if (this != NULL) { memcpy(append, this, thislength); }
+    if (num >= 0) { // execution
+        if (this) { // this != NULL
+            thislength = strlen(this); 
 
-        memset(append + thislength, c, num);
-        *(append + thislength + num) = '\0';
-            
-        this = term_str(this); // destructed original pointer
+            temp = this;
+            this = (char * )realloc(this, sizeof(char) * (thislength + num + 1));
+        
+            if (this) { // execution
+                memcpy(this, temp, thislength);
+                memset(this + thislength, c, num);
+                *(this + thislength + num) = '\0';
+            } else { // exception
+                temp = term_str(temp);
+                printf("memory is not allocated!");
+                exit(EXIT_FAILURE);
+            }
+        } else { // this == NULL
+            this = (char * )malloc(sizeof(char) * (num + 1));
+        
+            if (this) { // execution
+                memset(this, c, num);
+                *(this + num) = '\0';
+            } else { // exception
+                printf("memory is not allocated!");
+                exit(EXIT_FAILURE);
+            }
+        }
     } else { // exception
-        printf("memory is not allocated!");
+        printf("char: out of bounds!");
         exit(EXIT_FAILURE);
     }
 
-    return append;   
+    return this;   
 }
 
 char * apnd_fsub_str_h(char * this, const char * str, int32 len) { // function name + "_h" (meaning: heap memory(free() needed))
-    char * append = NULL;
+    char * temp = NULL;
     uint64 thislength, strlength;
 
-    if (str != NULL) { // execution
+    if (str) { // execution
         strlength = strlen(str);
     
         if (strlength >= len) { // execution
-            if (this != NULL) { thislength = strlen(this); } 
-            else              { thislength = 0; }
-        
-            append = (char * )malloc(sizeof(char) * (thislength + len + 1));
-        
-            if (append != NULL) { // execution
-                if (this != NULL) { memcpy(append, this, thislength); }             
+            if (this) {
+                thislength = strlen(this); 
 
-                memcpy(append + thislength, str, len);
-                *(append + thislength + len) = '\0';
+                temp = this;
+                this = (char * )realloc(this, sizeof(char) * (thislength + len + 1));
+        
+                if (this) { // execution
+                    memcpy(this, temp, thislength);                 
+                    memcpy(this + thislength, str, len);
+                    *(this + thislength + len) = '\0';
+                } else { // exception
+                    printf("memory is not allocated!");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                this = (char * )malloc(sizeof(char) * (len + 1));
                 
-                this = term_str(this); // destructed original pointer
-            } else { // exception
-                printf("memory is not allocated!");
-                exit(EXIT_FAILURE);
+                if (this) { // execution
+                    memcpy(this, str, len);
+                    *(this + len) = '\0';
+                } else { // exception
+                    printf("memory is not allocated!");
+                    exit(EXIT_FAILURE);
+                }
             }
         } else { // exception
             printf("st2: out of bounds!");
             exit(EXIT_FAILURE);
         }
-    } else { // exception
-        append = this;
-    } 
+    }
 
-    return append;   
+    return this;   
 }
 
 char * apnd_isub_str_h(char * this, const char * str, int32 index, int32 len) { // function name + "_h" (meaning: heap memory(free() needed))
-    char * append = NULL;
+    char * temp = NULL;
     uint64 thislength, strlength;
 
-    if (str != NULL) { // execution
+    if (str) { // execution
         strlength = strlen(str);
     
-        if (strlength >= (index + len)) { // execution
-            if (this != NULL) { thislength = strlen(this); } 
-            else              { thislength = 0; }
-        
-            append = (char * )malloc(sizeof(char) * (thislength + len + 1));
-        
-            if (append != NULL) { // execution
-                if (this != NULL) { memcpy(append, this, thislength); }             
+        if (index >= 0 && len > 0 && strlength >= (index + len)) { // execution
+            if (this) { // this != NULL
+                thislength = strlen(this); 
 
-                memcpy(append + thislength, str + index, len);
-                *(append + thislength + len) = '\0';
+                temp = this;
+                this = (char * )realloc(this, sizeof(char) * (thislength + len + 1));
+        
+                if (this) { // execution
+                    memcpy(this, temp, thislength);            
+                    memcpy(this + thislength, str + index, len);  
+                    *(this + thislength + len) = '\0';
+                } else { // exception
+                    printf("memory is not allocated!");
+                    exit(EXIT_FAILURE);
+                }
+            } else { // this == NULL
+                this = (char * )malloc(sizeof(char) * (len + 1));
                 
-                this = term_str(this); // destructed original pointer
-            } else { // exception
-                printf("memory is not allocated!");
-                exit(EXIT_FAILURE);
+                if (this) { //execution
+                    memcpy(this, str + index, len);
+                    *(this + len) = '\0';
+                } else { // exception
+                    printf("memory is not allocated!");
+                    exit(EXIT_FAILURE);
+                }
             }
         } else { // exception
             printf("st2: out of bounds!");
             exit(EXIT_FAILURE);
         }
-    } else { // exception
-        append = this;
-    } 
-
-    return append;   
+    }
+    
+    return this;   
 }
